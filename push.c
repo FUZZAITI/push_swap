d24e32933f4905f010723d5f898b0547b76aba97
FUZZAITI <pedrofuzzaiti@gmail.com>
2026-01-29 17:22:06 -0300

    int *

--- push.c
+++ push.c
@@ -1,30 +1,40 @@
 #include "push_swap.h"
+int *parse_args(int len,char **lista);
 
-void  parse_args(char **lista)
+
+int *parse_args(int len,char **lista)
 {
-    int i;
-    int *lista_nbr;
-
-    i = is_num(lista);
-    printf("lista só com numeros e %d\n",i);
-    is_valid(lista);
-    printf("numeros entre os inteiros\n");
+    char **nuns;
+    int   is_split;
+   
+    is_split = 0;
+    if (len == 2)
+    {
+        nuns = ft_split(lista[1], ' ');
+        is_split = 1;
+        if (!nuns || !nuns[0])
+            management_error(1, nuns);
+    }
+    else
+        nuns = lista + 1;    
+    if (!is_num(nuns))
+        management_error(is_split, nuns);
+    if (!is_valid(nuns))
+        management_error(is_split, nuns);
+    if (!has_duplicates(nuns))
+        management_error(is_split, nuns);
+    return (nrb_array(is_split,nuns));                
 }
 
 
 
 int main(int arvc, char** argv)
 {
-    char **nuns;
+    int *array_nbr;
     if (arvc < 2)
-        error_exit(); 
-    if (arvc == 2)
-    {
-        nuns = ft_split(argv[1], ' ');
-        if (!nuns || !nuns[0])
-           error_exit();
-    }
-    else
-        nuns = argv + 1;
-    parse_args(nuns);
+        return (0);
+         
+    array_nbr = parse_args(arvc,argv);
+    printf("aqui"); 
+    
 }
