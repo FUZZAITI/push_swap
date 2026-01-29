97ba6143d6e77a48b8b017abd196be365d879ac9
FUZZAITI <pedrofuzzaiti@gmail.com>
2026-01-29 17:22:34 -0300

    funções adicionadas

--- push_swap.h
+++ push_swap.h
@@ -8,13 +8,21 @@
 
 
 
-int main(int arvc, char** argv);
-char            **ft_split(const char *s,char c);
-size_t    ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t	ft_strlen(const char *s);
-long ft_atol(const char *str);
-int is_num(char **s);
-int is_valid(char **list);
-void error_exit(void);
+int         main(int arvc, char** argv);
+char      **ft_split(const char *s,char c);
+size_t      ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	    ft_strlen(const char *s);
+long        ft_atol(const char *str);
+int         is_num(char **s);
+int         is_valid(char **list);
+void        error_exit(void);
+void        management_error(int i, char **s);
+void        free_list(char **lista);
+int         has_duplicates(char **nums);
+int        *parse_args(int len,char **lista);
+int        *nrb_array(int is_split,char **list);
+
+
+
 
 #endif
